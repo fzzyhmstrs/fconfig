28ae694c10b7aedaaeb5d9174bf4601f1a9cbbaa
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2023-08-02 14:53:16 -0400

    Update ValidatedArmorMaterial.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field/ValidatedArmorMaterial.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field/ValidatedArmorMaterial.kt
@@ -50,7 +50,16 @@ open class ValidatedArmorMaterial protected constructor(
         return toughness.get()
     }
 
-    open class Builder(protected val name: String, protected val soundEvent: SoundEvent){
+    class Builder(name: String, soundEvent: SoundEvent): AbstractBuilder<ValidatedToolMaterial, Builder>{
+        override fun builderClass(): Builder{
+            return this
+        }
+        override fun build(): ValidatedArmorMaterial {
+            return ValidatedArmorMaterial(name, soundEvent, rI, e, pA, dM, kR, t)
+        }
+    }
+    
+    abstract class AbstractBuilder(protected val name: String, protected val soundEvent: SoundEvent)<T: ArmorMaterial, U: AbstractBuilder>{
         protected var rI = ValidatedIngredient(Ingredient.empty())
         protected var e = ValidatedInt(1,50,0)
         protected var pA = ValidatedSeries(arrayOf(1,1,1,1),Int::class.java) {a,b -> a >= 0 && b >= 0}
@@ -58,34 +67,33 @@ open class ValidatedArmorMaterial protected constructor(
         protected var kR = ValidatedFloat(0f,0.25f,0f)
         protected var t = ValidatedFloat(0f,1f,0f)
 
+        abstract fun builderClass(): U
         
-        fun repairIngredient(ingredient: Ingredient): Builder{
+        fun repairIngredient(ingredient: Ingredient): U{
             rI = ValidatedIngredient(ingredient)
-            return this
+            return builderClass()
         }
-        fun enchantability(default: Int, max: Int = 50): BUilder{
+        fun enchantability(default: Int, max: Int = 50): U{
             e = ValidatedInt(default,max,1)
-            return this
+            return builderClass()
         }
-        fun protectionAmounts(helmet: Int, chestplate: Int, leggings: Int, boots: Int): Builder{
+        fun protectionAmounts(helmet: Int, chestplate: Int, leggings: Int, boots: Int): U{
             pA = ValidatedSeries(arrayOf(boots,leggings,chestplate,helmet),Int::class.java) {a,b -> a >= 0 && b >= 0}
-            return this
+            return builderClass()
         }
-        fun durabilityMultiplier(default: Int, max: Float = 100f): Builder{
+        fun durabilityMultiplier(default: Int, max: Float = 100f): U{
             dM = ValidatedInt(default, max, 1)
-            return this
+            return builderClass()
         }
-        fun knockbackReistance(default: Float): Builder{
+        fun knockbackReistance(default: Float): U{
             kR = ValidatedFloat(default,0.25f,0f)
-            return this
+            return builderClass()
         }
-        fun toughness(default: Float, max: Float = 5f): Builder{
+        fun toughness(default: Float, max: Float = 5f): U{
             t = ValidatedFloat(default,max,0f)
-            return this
+            return builderClass()
         }
         
-        open fun build(): ValidatedArmorMaterial{
-            return ValidatedArmorMaterial(name, soundEvent, rI, e, pA, dM, kR, t)
-        }
+        abstract fun build(): T
     }
 }
