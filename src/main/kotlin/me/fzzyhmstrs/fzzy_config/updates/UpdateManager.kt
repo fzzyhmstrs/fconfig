05c6b7af95c13277aa6993784e13686bb756b866
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-05 15:28:35 -0500

    Update UpdateManager.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
@@ -5,9 +5,14 @@ import me.fzzyhmstrs.fzzy_config.util.Update
 object UpdateManager{
 
     private val updateMap: MutableMap<String, Updater>
+    private val changeHistory: LinkedList<Text> = LinkedList()
     
     fun update(key: String, update: Update){
         updateMap[key].update(update)
+    }
+
+    fun addUpdateMessage(text: Text){
+        changeHistory.push(text)
     }
     
 }
