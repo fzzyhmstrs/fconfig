3d3bee6c61c1d469a933a2c389234d626056c2ef
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-05 11:28:24 -0500

    Create DirtyMarkableContaining.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/interfaces/DirtyMarkableContaining.kt
@@ -0,0 +1,17 @@
+package me.fzzyhmstrs.fzzy_config.interfaces
+
+import org.jetbrains.annotations.ApiStatus.Internal
+import java.util.concurrent.Callable
+
+/**
+ * Implementing class can mark itself dirty, as well as pass that state along to listeners
+ *
+ * Internal to FzzyConfig. Used by [ValidatedField] and [ConfigSection].
+ *
+ * @author fzzyhmstrs
+ * @since 0.2.0
+ */
+@Internal
+interface DirtyMarkableContaining {
+    fun update(update: Callable<ValidationResult<Boolean>>
+}
