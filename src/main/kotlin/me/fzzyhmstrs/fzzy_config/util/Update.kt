86c903aa24dad2f293cf56cee1f73f08a9d51cd7
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-05 14:46:10 -0500

    Update Update.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/Update.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/Update.kt
@@ -3,4 +3,4 @@ package me.fzzyhmstrs.fzzy_config.util
 import java.util.concurent.Callable
 import net.minecraft.text.Text
 
-data class Update(val desc: Text, val undo: Callable<ValidationResult<Text>>, val redo: Callable<ValidationResult<Text>>)
+data class Update(val desc: Text, val undo: Callable<Text>, val redo: Callable<Text>)
