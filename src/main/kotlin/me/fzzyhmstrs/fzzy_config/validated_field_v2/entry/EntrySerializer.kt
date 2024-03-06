b47386b5a666504d5bb39188a13ba944df515330
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-06 12:36:43 -0500

    Create EntrySerializer.kt

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field_v2/entry/EntrySerializer.kt
@@ -0,0 +1,15 @@
+package me.fzzyhmstrs.fzzy_config.validated_field_v2.entry
+
+import net.peanuuutz.tomlkt.TomlElement
+
+/**
+ * Deserializes individual entries in a complex [ValidatedField]
+ *
+ * SAM: [deserialize] takes a TomlElement, returns a deserialized instance of T
+ * @author fzzyhmstrs
+ * @since 0.1.1
+ */
+@FunctionalInterface
+fun interface EntrySerializer<T> {
+    fun serializeEntry(entry: T): TomlElement
+}
