b9fc7687a601addfb1fa595bbb5c97d899f9dcd6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-06 12:35:02 -0500

    Update EntryDeserializer.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field_v2/entry/EntryDeserializer.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field_v2/entry/EntryDeserializer.kt
@@ -3,13 +3,12 @@ package me.fzzyhmstrs.fzzy_config.validated_field_v2.entry
 import net.peanuuutz.tomlkt.TomlElement
 
 /**
-     * Deserializes individual entries in a complex [ValidatedField]
-     *
-     * SAM: [deserialize] takes a TomlElement, returns a deserialized instance of T
-     * @author fzzyhmstrs
-     * @since 0.1.1
-     */
-    fun interface EntryDeserializer<T> {
-        fun deserialize(toml: TomlElement): T
-    }
+ * Deserializes individual entries in a complex [ValidatedField]
+ *
+ * SAM: [deserialize] takes a TomlElement, returns a deserialized instance of T
+ * @author fzzyhmstrs
+ * @since 0.1.1
+ */
+fun interface EntryDeserializer<T> {
+    fun deserializeEntry(toml: TomlElement, fieldName: String): ValidationResult<T>
 }
