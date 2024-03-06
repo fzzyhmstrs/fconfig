01ea899cf16a77e078b669037bdb20117eeee149
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-06 14:42:22 -0500

    Update EntryHandler.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field_v2/entry/EntryHandler.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validated_field_v2/entry/EntryHandler.kt
@@ -1,3 +1,3 @@
 package me.fzzyhmstrs.fzzy_config.validated_field_v2.entry
 
-fun interface EntryHandler<T>: EntrySerializer<T>, EntryDeserializer<T>, EntryValidator<T>, EntryCorrector<T>
+interface EntryHandler<T>: EntrySerializer<T>, EntryDeserializer<T>, EntryValidator<T>, EntryCorrector<T>
