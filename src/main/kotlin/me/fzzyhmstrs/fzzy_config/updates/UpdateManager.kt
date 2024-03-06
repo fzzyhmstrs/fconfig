1ade1e33f4dbcb1c85d4f7d658870b38e13bb76e
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-06 11:18:08 -0500

    Update UpdateManager.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
@@ -8,17 +8,47 @@ import java.util.*
 
 object UpdateManager{
 
+    // Important Base Concept: SCOPE
+    // basically a string mapping of the "location" of an element in a config layout, not disimilar to a file path
+    //
+    // Top level
+    //   The namespace of the mod adding configs. The namespace of config.getId()
+    //   ex. 'mymod'
+    //
+    // Config
+    //   Next level is the config name, the path of the getId()
+    //   ex. 'items'
+    //
+    // Subsection
+    //   sections add a layer to the scope. stacks.
+    //   ex. 'dropRates'
+    //   
+    // Element
+    //   finally the element terminates the scope
+    //   ex. 'oceanChests'
+    //
+    // Built
+    //   scopes are built into translation-key-like strings
+    //   ex. 'mymod.items.dropRates.oceanChests'
+
     private val updateMap: MutableMap<String, Updater> = mutableMapOf()
     private val changeHistory: LinkedList<Text> = LinkedList()
+    private val changeHistory: MutableMap<String, ArrayListMultimap<Long, Text>>
     private var changeCount = 0
+    private var currentScope = ""
+    
+    fun setScope(scope: String){
+        currentScope = scope
+    }
 
-    fun needsUpdatePop(key: String, updatable: Updatable): Boolean {
+    fun needsUpdatePop(updatable: Updatable): Boolean {
+        val key = updatable.getUpdateKey()
         val updater = updateMap[key] ?: return false
         if (!updater.canUndo()) return false
         return updatable.popState()
     }
 
-    fun needsUpdatePeek(key: String, updatable: Updatable): Boolean {
+    fun needsUpdatePeek(updatable: Updatable): Boolean {
         val updater = updateMap[key] ?: return false
         if (!updater.canUndo()) return false
         return updatable.peekState()
@@ -29,20 +59,44 @@ object UpdateManager{
         changeCount++
     }
 
-    fun updateChangeCount(){
+    fun getChangeCount(scope: String): Int {
         var count = 0
-        for (updater in updateMap.values){
-            count += updater.changeCount()
+        for (update in getScopedUpdates(scope)){
+            count += update.changeCount()
         }
-        changeCount = count
+        return count
     }
 
-    fun addUpdateMessage(text: Text){
+    fun updateCurrentChangeCount(): Int {
+        return getChangeCount(currentScope)
+    }
+
+    fun revert(scope: String) {
+        for (update in getScopedUpdates(scope)){
+            update.revert()
+        }        
+    }
+
+    fun revertCurrent() {
+        revert(currentScope)
+    }
+
+    fun addUpdateMessage(key: String,text: Text) {
         changeHistory.push(text)
+    }
+
+    fun getScopedUpdates(scope: String): Collection<Updater> {
+        return (if(scope.isEmpty()) updateMap.keys else updateMap.keys.filter{ it.startsWith(scope) }).mapNotNull { updateMap[it] }
     }
 
     fun<T: Config> applyKeys(config: T) {
         ConfigApiImpl.walk(config,config.getId().toShortTranslationKey(),true) {str, v -> if (v is Updatable) v.setUpdateKey(str)}
+    }
+
+    fun<T: Config> getSyncUpdates(config: T): Map<String, FzzySerializable> {
+        val map: MutableMap<String, FzzySerializable> = mutableMapOf()
+        ConfigApiImpl.walk(config,config.getId().toShortTranslationKey(),false) {str, v -> if (v is Updatable && v is FzzySerializable) { if (needsUpdatePop(v)) map[str] = v }}
+        return map
     }
 
 }
