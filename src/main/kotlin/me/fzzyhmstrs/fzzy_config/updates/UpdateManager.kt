2d995430c6c4f78ed08964b87a42f7d23d514e7d
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-06 15:07:42 -0500

    Update UpdateManager.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/UpdateManager.kt
@@ -108,9 +108,9 @@ object UpdateManager{
         ConfigApiImpl.walk(config,config.getId().toShortTranslationKey(),true) {str, v -> if (v is Updatable) v.setUpdateKey(str)}
     }
 
-    fun<T: Config> getSyncUpdates(config: T): Map<String, FzzySerializable> {
+    fun<T: Config> getSyncUpdates(config: T, ignoreNonSync: Boolean = false): Map<String, FzzySerializable> {
         val map: MutableMap<String, FzzySerializable> = mutableMapOf()
-        ConfigApiImpl.walk(config,config.getId().toShortTranslationKey(),false) {str, v -> if (v is Updatable && v is FzzySerializable) { if (needsUpdatePop(v)) map[str] = v }}
+        ConfigApiImpl.walk(config,config.getId().toShortTranslationKey(), ignoreNonSync) {str, v -> if (v is Updatable && v is FzzySerializable) { if (needsUpdatePop(v)) map[str] = v }}
         return map
     }
 
