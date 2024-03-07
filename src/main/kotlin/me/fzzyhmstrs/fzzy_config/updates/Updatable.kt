17f9a5d498c3e6ddcce2358d317d19983b685d13
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-07 08:56:54 -0500

    Update Updatable.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/Updatable.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/updates/Updatable.kt
@@ -12,7 +12,7 @@ import org.jetbrains.annotations.ApiStatus.Internal
  * @since 0.2.0
  */
 @Internal
-interface Updatable {
+internal interface Updatable {
     fun getUpdateKey(): String //returned by the Updatable element to denote its place in the heirarchy.
     fun setUpdateKey(key: String) //used by the config validator to set the elements key. Only done on CONFIGURATION sync on the client side. UpdateManager doesn't do ish on the Server
     fun update(update: Update){ //pushes an update to the UpdateManager based on its key, so the manager can track individual change "threads"
