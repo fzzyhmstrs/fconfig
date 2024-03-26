4ec43b87a07e60b75b43be1ef28a75860534080f
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-03-26 14:58:22 -0400

    Update ValidatedString.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedString.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedString.kt
@@ -8,6 +8,15 @@ import net.peanuuutz.tomlkt.TomlElement
 import net.peanuuutz.tomlkt.TomlLiteral
 import net.peanuuutz.tomlkt.asTomlLiteral
 
+/*
+ * A validated string value
+ * @param defaultValue String default for the setting
+ * @param checker [EntryChecker] defining validation and correction for the string inputs.
+ * @see [Builder]
+ * @sample [me.fzzyhmstrs.fzzy_config.examples.ValidatedMiscExamples.validatedString]
+ * @author fzzyhmstrs
+ * @since 0.2.0
+ */
 class ValidatedString(defaultValue: String, private val checker: EntryChecker<String>): ValidatedField<String>(defaultValue) {
 
     override fun copyStoredValue(): String {
@@ -42,6 +51,13 @@ class ValidatedString(defaultValue: String, private val checker: EntryChecker<St
         return ValidatedString(String(defaultValue.toCharArray()), this.checker)
     }
 
+    /*
+     * A validated string builder, integrated with an EntryChecker builder
+     * @param defaultValue the default String value
+     * @sample [me.fzzyhmstrs.fzzy_config.examples.ValidatedMiscExamples.validatedString]
+     * @author fzzyhmstrs
+     * @since 0.2.0
+     */
     class Builder(private val defaultValue: String): EntryValidator.AbstractBuilder<String, Builder>() {
         override fun builder(): Builder {
             return this
