1fcd51d965dcbdb2d1a3aec62a81b7577eb3e161
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-04-02 11:41:03 -0400

    Update SuppliedPos.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/SuppliedPos.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/SuppliedPos.kt
@@ -2,7 +2,15 @@ package me.fzzyhmstrs.fzzy_config.util.pos
 
 import java.util.function.Supplier
 
-class SuppliedPos(private val parent: Pos, private var p: Int = 0, private val offset: Supplier<Int>): Pos {
+/**
+ * A relative [Pos] wth a secondary offset supplier. Offsets a parent Pos. Mutation of this pos will alter the offset.
+ * @param parent Pos - the Pos this is relative to
+ * @param p Int - the offset compared to the parent
+ * @param offset Supplier<Int> - the secondary supplied offset compared to the parent
+ * @author fzzyhmstrs
+ * @since 0.2.0
+ */
+class SuppliedPos(private val parent: Pos, private var p: Int, private val offset: Supplier<Int>): Pos {
     override fun get(): Int {
         return parent.get() + p + offset.get()
     }
