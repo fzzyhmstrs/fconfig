39f35a433af554d3ba6270b729e20610d038aa80
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-04-02 11:30:34 -0400

    Update Pos.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/Pos.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/Pos.kt
@@ -1,8 +1,33 @@
 package me.fzzyhmstrs.fzzy_config.util.pos
 
+/**
+ * Defines the mutable position of something
+ * @author fzzyhmstrs
+ * @since 0.2.0
+ */
 interface Pos {
+    /**
+     * Returns the position of this pos
+     * @author fzzyhmstrs
+     * @since 0.2.0
+     */
     fun get(): Int
+    /**
+     * Sets the position of this pos
+     * @author fzzyhmstrs
+     * @since 0.2.0
+     */
     fun set(new: Int)
+    /**
+     * Increments this position by the given amount
+     * @author fzzyhmstrs
+     * @since 0.2.0
+     */
     fun inc(amount: Int)
+    /**
+     * Decrements this position by the given amount
+     * @author fzzyhmstrs
+     * @since 0.2.0
+     */
     fun dec(amount: Int)
 }
