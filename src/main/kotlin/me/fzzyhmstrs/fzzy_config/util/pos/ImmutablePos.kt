6f7bda2091dd78ffc4b0ca55650d48fe54cb4297
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-04-02 11:33:43 -0400

    Update ImmutablePos.kt

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/ImmutablePos.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/ImmutablePos.kt
@@ -1,5 +1,10 @@
 package me.fzzyhmstrs.fzzy_config.util.pos
 
+/**
+ * An immutable [Pos]. Typically used as a position "anchor" in other positions
+ * @author fzzyhmstrs
+ * @since 0.2.0
+ */
 class ImmutablePos(private val p: Int = 0): Pos {
     override fun get(): Int {
         return p
