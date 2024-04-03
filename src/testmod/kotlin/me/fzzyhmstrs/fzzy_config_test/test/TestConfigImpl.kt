68a4f142477f632cd4eb1cb82a4bc60e0c393f9c
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-04-03 14:10:17 -0400

    Update TestConfigImpl.kt

--- src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl.kt
+++ src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl.kt
@@ -1,9 +1,17 @@
 package me.fzzyhmstrs.fzzy_config_test.test
 
 import me.fzzyhmstrs.fzzy_config.config.Config
+import me.fzzyhmstrs.fzzy_config.validation.Shorthand.validated
 
-class TestConfigImpl: Config("test_config", "fzzy_config","me/fzzyhmstrs/fzzy_config_test/testzzyhmstrs/fzzy_config_test/test") {
+class TestConfigImpl: Config("test_config", "fzzy_config", "test") {
 
+    var bl1 = true
+    var bl2 = ValidatedBoolean(true)
 
+    var int1 = 6
+    var int2 = ValidatedInt(6,10,1)
+
+    var enum1 = TestEnum.ALPHA
+    var enum2 = TestEnum.BETA.validated()
 
 }
