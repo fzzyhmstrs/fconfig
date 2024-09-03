b51e0e879fcb86e5f5813d00e15073d1722e45a1
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-03 11:46:52 -0400

    Update TestConfigImpl4.kt
    (cherry picked from commit ee78f4a7da8a004bbb593f7fd86a0ac8001b9dae)

--- src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl4.kt
+++ src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl4.kt
@@ -10,6 +10,8 @@
 
 package me.fzzyhmstrs.fzzy_config_test.test
 
+import me.fzzyhmstrs.fzzy_config.annotations.WithCustomPerms
+import me.fzzyhmstrs.fzzy_config.annotations.RequiresAction
 import me.fzzyhmstrs.fzzy_config.annotations.RequiresRestart
 import me.fzzyhmstrs.fzzy_config.config.Config
 import me.fzzyhmstrs.fzzy_config.validation.collection.ValidatedList
@@ -20,14 +22,16 @@ import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedBoolean
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedString
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedDouble
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
+import me.fzzyhmstrs.fzzy_config_test.FC
+import me.fzzyhmstrs.fzzy_config_test.FC.TEST_PERMISSION_BAD
 import net.fabricmc.loader.api.FabricLoader
 import net.minecraft.entity.attribute.EntityAttributeModifier
 import net.minecraft.util.Identifier
 
 @RequiresRestart
-class TestConfigImpl4: Config(Identifier("fzzy_config_test", "test_config4")) {
+class TestConfigImpl4: Config(Identifier.of("fzzy_config_test","test_config4")) {
 
-
+    @WithCustomPerms([TEST_PERMISSIONS_BAD])
     var bl1 = true
     var bl2 = ValidatedBoolean()
 
@@ -35,7 +39,8 @@ class TestConfigImpl4: Config(Identifier("fzzy_config_test", "test_config4")) {
     var int1 = 6
     var int2 = ValidatedInt(6, 10, 1)
 
-    var mapDouble = ValidatedStringMap(mapOf("a" to 1.0), ValidatedString(), ValidatedDouble(1.0, 1.0, 0.0))
+    @RequiresAction(Action.RELOG)
+    var mapDouble = ValidatedStringMap(mapOf("a" to 1.0),ValidatedString(), ValidatedDouble(1.0,1.0,0.0))
 
     var namespaceBlackList: ValidatedList<String> = ValidatedString.fromList(FabricLoader.getInstance().allMods.map{ it.metadata.id }).toList()
 
@@ -43,8 +48,7 @@ class TestConfigImpl4: Config(Identifier("fzzy_config_test", "test_config4")) {
 
     var exampleValidatedAttribute1 = ValidatedEntityAttribute.Builder("generic.max_health", true)
         // supply a UUID and name, otherwise generic ones will be used for you
-        .uuid("f68e98a2-0599-11ef-9262-0242ac120002")
-        .name("My Example ValidatedEntityAttribute")
+        .id(Identifier.of(FC.MOD_ID,"test_attribute_1"))
         //set amount, and optionally provide a range restriction
         .amount(1.0, 0.0, 8.0)
         //set the operation for the modifier, and optionally lock the modifier to the operation chosen
@@ -54,8 +58,7 @@ class TestConfigImpl4: Config(Identifier("fzzy_config_test", "test_config4")) {
 
     var exampleValidatedAttribute2 = ValidatedEntityAttribute.Builder("generic.max_health", false)
         // supply a UUID and name, otherwise generic ones will be used for you
-        .uuid("8563c5ba-059b-11ef-9262-0242ac120002")
-        .name("My Example ValidatedEntityAttribute")
+        .id(Identifier.of(FC.MOD_ID,"test_attribute_2"))
         //set amount, and optionally provide a range restriction
         .amount(0.1, 0.0, 1.0)
         //set the operation for the modifier, and optionally lock the modifier to the operation chosen
