147debc39ae7479c7c5a4c44deb51dd467d0cee6
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-06 17:15:58 -0400

    add full-exit back button behavior

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/screen/widget/internal/DoneButtonWidget.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2024 Fzzyhmstrs
+ *
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+ *
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * You should have received a copy of the TDL-M with this software.
+ * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ */
+
+package me.fzzyhmstrs.fzzy_config.screen.widget.internal
+
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.ButtonWidget
+import net.minecraft.screen.ScreenTexts
+import net.minecraft.text.Text
+
+class DoneButtonWidget(onPress: PressAction): ButtonWidget(0, 0, 78, 20, ScreenTexts.DONE, onPress, DEFAULT_NARRATION_SUPPLIER) {
+
+    override fun getMessage(): Text {
+        return if (Screen.hasShiftDown()) {
+            ScreenTexts.DONE
+        } else {
+            super.getMessage()
+        }
+    }
+
+}
