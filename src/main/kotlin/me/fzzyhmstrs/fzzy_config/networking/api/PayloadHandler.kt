97d0d42d17859e98671a97bce8e6e359c0023454
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-09 12:52:39 -0400

    implement lightweight networking abstraction

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/networking/api/PayloadHandler.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2024 Fzzyhmstrs
+ *
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+ *
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * You should have received a copy of the TDL-M with this software.
+ * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ */
+
+package me.fzzyhmstrs.fzzy_config.networking.api
+
+import net.minecraft.entity.player.PlayerEntity
+import net.minecraft.network.packet.CustomPayload
+
+interface PayloadHandler<T: CustomPayload, P: PlayerEntity, C: NetworkContext<P>> {
+    fun handle(payload: T, context: C)
+}
