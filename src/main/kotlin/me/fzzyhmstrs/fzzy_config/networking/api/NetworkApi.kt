3c3971b864ec0fa37c0479973e6cfcfad8815945
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-09 15:55:25 -0400

    port to 1.20.1 forge start2

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/networking/api/NetworkApi.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/networking/api/NetworkApi.kt
@@ -14,10 +14,9 @@ import io.netty.buffer.Unpooled
 import me.fzzyhmstrs.fzzy_config.networking.FzzyPayload
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.network.PacketByteBuf
-import net.minecraft.network.RegistryByteBuf
-import net.minecraft.network.codec.PacketCodec
-import net.minecraft.network.packet.CustomPayload
 import net.minecraft.util.Identifier
+import java.util.function.BiConsumer
+import java.util.function.BiFunction
 import java.util.function.Function
 
 /**
@@ -27,6 +26,11 @@ import java.util.function.Function
  */
 interface NetworkApi {
 
+    /**
+     * Shorthand for creating a [PacketByteBuf]
+     * @author fzzyhmstrs
+     * @since 0.4.1
+     */
     fun buf(): PacketByteBuf {
         return PacketByteBuf(Unpooled.buffer())
     }
@@ -57,21 +61,23 @@ interface NetworkApi {
     /**
      * registers a clientbound (S2C) payload type and receipt handler. This must be done on both logical sides (client and server). A common entrypoint is typically the best place for this.
      * @param T the payload type to register
-     * @param id [CustomPayload.Id] the id of the custom payload
-     * @param codec [PacketCodec] the packet codec for serializing the custom payload
+     * @param writer [BiConsumer]&lt;[T],[PacketByteBuf]&gt; the consumer for writing serialized data into a buffer from a payload
+     * @param id [Identifier] the id of the custom payload
+     * @param function [Function]&lt;[PacketByteBuf],[T]&gt; the function for building the payload from a given buf
      * @param handler [S2CPayloadHandler] a handler for dealing with receiving the payload. This handler will be on the client handling a payload received from the server. As such, take care with your client-only class references, pushing them to a method to reference, perhaps.
      * @author fzzyhmstrs
      * @since 0.4.1
      */
-    fun <T: FzzyPayload> registerS2C(id: Identifier, function: Function<PacketByteBuf, T>, handler: S2CPayloadHandler<T>)
+    fun <T: FzzyPayload> registerS2C(id: Identifier, writer: BiConsumer<T, PacketByteBuf>, function: Function<PacketByteBuf, T>, handler: S2CPayloadHandler<T>)
     /**
      * registers a serverbound (C2S) payload type and receipt handler. This must be done on both logical sides (client and server). A common entrypoint is typically the best place for this.
      * @param T the payload type to register
-     * @param id [CustomPayload.Id] the id of the custom payload
-     * @param codec [PacketCodec] the packet codec for serializing the custom payload
+     * @param id [Identifier] the id of the custom payload
+     * @param writer [BiConsumer]&lt;[T],[PacketByteBuf]&gt; the consumer for writing serialized data into a buffer from a payload
+     * @param function [Function]&lt;[PacketByteBuf],[T]&gt; the function for building the payload from a given buf
      * @param handler [S2CPayloadHandler] a handler for dealing with receiving the payload. This handler will be on the server handling a payload sent from the client.
      * @author fzzyhmstrs
      * @since 0.4.1
      */
-    fun <T: FzzyPayload> registerC2S(id: Identifier, function: Function<PacketByteBuf, T>, handler: C2SPayloadHandler<T>)
+    fun <T: FzzyPayload> registerC2S(id: Identifier, writer: BiConsumer<T, PacketByteBuf>, function: Function<PacketByteBuf, T>, handler: C2SPayloadHandler<T>)
 }
