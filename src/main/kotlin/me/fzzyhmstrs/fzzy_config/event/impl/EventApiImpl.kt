8c8643746723f6e52ec6a59a0fc366036986cec9
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-09-24 09:41:26 -0400

    Create EventApiImpl.kt
    (cherry picked from commit f1de984978aa6291722e8667ba8535e8019b88e4)

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/event/impl/EventApiImpl.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Fzzyhmstrs
+ *
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+ *
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * You should have received a copy of the TDL-M with this software.
+ * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ */
+
+package me.fzzyhmstrs.fzzy_config.event.api
+
+import me.fzzyhmstrs.fzzy_config.config.Config
+import me.fzzyhmstrs.fzzy_config.event.api.*
+import net.minecraft.server.network.ServerPlayerEntity
+
+internal object EventApiImpl: EventApi {
+
+    private val onChangedClientListeners: MutableList<OnChangedClientListener> = mutableListOf()
+
+    private val onChangedServerListeners: MutableList<OnChangedServerListener> = mutableListOf()
+    
+    override fun onChangedClient(listener: OnChangedClientListener) {
+        onChangedClientListeners.add(listener)
+    }
+
+    override fun onChangedServer(listener: OnChangedServerListener) {
+        onChangedServerListeners.add(listener)
+    }
+
+    internal fun fireOnChangedClient(id: String, config: Config) {
+        for (listener in onChangedClientListeners) {
+            listener.onChanged(id, config)
+        }
+    }
+
+    internal fun fireOnChangedServer(id: String, config: Config, player: ServerPlayerEntity) {
+        for (listener in onChangedServerListeners) {
+            listener.onChanged(id, config, player)
+        }
+    }
+}
