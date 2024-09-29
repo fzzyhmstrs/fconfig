d744d81be453e39236de71d534d112bcb0f150d8
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-09-29 16:28:18 -0400

    Themes - add theme key

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/theme/ThemeKey.kt
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2024 Fzzyhmstrs
+ *
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+ *
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * You should have received a copy of the TDL-M with this software.
+ * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ */
+
+package me.fzzyhmstrs.fzzy_config.theme
+
+import com.mojang.serialization.MapCodec
+
+class ThemeKey<T: Any>(val codec: MapCodec<T>) {
+
+    fun provideValue(map: Map<ThemeKey<*>, *>): T? {
+        return map[this] as? T
+    }
+}
