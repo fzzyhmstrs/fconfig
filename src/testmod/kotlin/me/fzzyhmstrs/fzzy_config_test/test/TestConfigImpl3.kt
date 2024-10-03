f1a09074b43958ec98f08f7a1adb1d5dbf30d568
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-10-03 18:10:49 -0400

    fix infinite while 4 real, improve increment logic, fix renderWidget SOE
    (cherry picked from commit 1d611fecaecbcde696f22844ab7fefca5a8b14df)

--- src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl3.kt
+++ src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImpl3.kt
@@ -18,6 +18,8 @@ import me.fzzyhmstrs.fzzy_config.config.ConfigAction
 import me.fzzyhmstrs.fzzy_config.screen.widget.TextureIds
 import me.fzzyhmstrs.fzzy_config.util.FcText.lit
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedBoolean
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedDouble
+import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedFloat
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedInt
 import net.minecraft.block.Blocks
 import net.minecraft.client.MinecraftClient
@@ -59,6 +61,8 @@ class TestConfigImpl3: Config(Identifier.of("fzzy_config_test","test_config3")) 
     }
     @RequiresRestart
     private var int2 = ValidatedInt(6, 10, 1)
+
+    private var floatTest = ValidatedDouble(5.0, 6.6, 5.0)
 
     private var itemTest = Items.EGG
 
