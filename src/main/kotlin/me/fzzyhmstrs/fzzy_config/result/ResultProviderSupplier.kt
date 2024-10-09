f95baa338ae34a1341ba9bffa86000976a1141c6
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-10-09 09:40:58 -0400

    Update ResultProviderSupplier.kt
    (cherry picked from commit 61ef43e5380320f9d6a7bf4544b5c5cb1003099b)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplier.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplier.kt
@@ -14,7 +14,27 @@ import me.fzzyhmstrs.fzzy_config.config.Config
 import java.util.function.Supplier
 import kotlin.reflect.KMutableProperty
 
+/**
+ * Builds a [Supplier] of values from a config
+ * @param T non-null type to build a supplier of
+ * @author fzzyhmstrs
+ * @since 0.5.3
+ */
 @FunctionalInterface
 fun interface ResultProviderSupplier<T: Any> {
+    /**
+     * Creates a result supplier from config values. Since configs are by design mutable in-game, the result should freshly inspect the value on every `get()`.
+     * - If the `thing` is a `ValidatedField` or otherwise a supplier of values, it can be used directly to provide a supplier of values
+     * - If the `thing` is a basic primitive or object, the `property` should be used to supply values from the properties getter
+     *
+     * Using this general strategy ensures that fresh values are supplied on every call.
+     * @param scope String representation of a config setting. See [the translation wiki page](https://github.com/fzzyhmstrs/fconfig/wiki/Translation) for an overview of how scopes work.
+     * @param config [Config] the config instance providing results
+     * @param thing [Any] the candidate result to potentially use to create the supplier with.
+     * @param proprty [KMutableProperty] reflection property instance for the [thing]
+     * @return [Supplier]&lt;[T]&gt; supplier instance for retrieving values from the config scope provided.
+     * @author fzzyhmstrs
+     * @since 0.5.3
+     */
     fun supplier(scope: String, config: Config, thing: Any, property: KMutableProperty<*>): Supplier<T>
 }
