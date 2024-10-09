dff878a27a87a72a3b5331408a8487257a8184f5
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-10-09 09:46:17 -0400

    Update ResultProviderSupplierJava.kt
    (cherry picked from commit 73360b80033452ce177dd301e521b54028ed044b)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplierJava.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplierJava.kt
@@ -14,8 +14,28 @@ import me.fzzyhmstrs.fzzy_config.config.Config
 import java.lang.reflect.Field
 import java.util.function.Supplier
 
+/**
+ * Builds a [Supplier] of values from a config (java friendly)
+ * @param T non-null type to build a supplier of
+ * @author fzzyhmstrs
+ * @since 0.5.3
+ */
 @FunctionalInterface
-@JvmDefaultWithoutCompatibility
 fun interface ResultProviderSupplierJava<T: Any> {
+    
+    /**
+     * Creates a result supplier from config values. Since configs are by design mutable in-game, the result should freshly inspect the value on every `get()`.
+     * - If the `thing` is a `ValidatedField` or otherwise a supplier of values, it can be used directly to provide a supplier of values
+     * - If the `thing` is a basic primitive or object, the `property` should be used to supply values from the field getter
+     *
+     * Using this general strategy ensures that fresh values are supplied on every call.
+     * @param scope String representation of a config setting. See [the translation wiki page](https://github.com/fzzyhmstrs/fconfig/wiki/Translation) for an overview of how scopes work.
+     * @param config [Config] the config instance providing results
+     * @param thing [Any] the candidate result to potentially use to create the supplier with.
+     * @param proprty [Field], nullable - reflection field instance for the [thing]
+     * @return [Supplier]&lt;[T]&gt; supplier instance for retrieving values from the config scope provided.
+     * @author fzzyhmstrs
+     * @since 0.5.3
+     */
     fun supplierJava(scope: String, config: Config, thing: Any, property: Field?): Supplier<T>
 }
