9cdaa20e4339ae988ba425ba07f1f402c7cf1f33
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-10-09 21:58:52 -0400

    get first tests of result provider system working, and start integration of an args system for lists, maps, etccc.
    (cherry picked from commit 2dfa5c4e44007e378a42c192694cbaceb2c6a7b4)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplierJava.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/result/ResultProviderSupplierJava.kt
@@ -11,6 +11,7 @@
 package me.fzzyhmstrs.fzzy_config.result
 
 import me.fzzyhmstrs.fzzy_config.config.Config
+import org.jetbrains.annotations.ApiStatus
 import java.lang.reflect.Field
 import java.util.function.Supplier
 
@@ -21,8 +22,9 @@ import java.util.function.Supplier
  * @since 0.5.3
  */
 @FunctionalInterface
+@ApiStatus.Experimental
 fun interface ResultProviderSupplierJava<T: Any> {
-    
+
     /**
      * Creates a result supplier from config values. Since configs are by design mutable in-game, the result should freshly inspect the value on every `get()`.
      * - If the `thing` is a `ValidatedField` or otherwise a supplier of values, it can be used directly to provide a supplier of values
@@ -30,12 +32,14 @@ fun interface ResultProviderSupplierJava<T: Any> {
      *
      * Using this general strategy ensures that fresh values are supplied on every call.
      * @param scope String representation of a config setting. See [the translation wiki page](https://github.com/fzzyhmstrs/fconfig/wiki/Translation) for an overview of how scopes work.
+     * @param args List&lt;String&gt; optionally provided args appended to the provided scope. The scope parameter also technically contains these, pre-processing
      * @param config [Config] the config instance providing results
      * @param thing [Any] the candidate result to potentially use to create the supplier with.
-     * @param proprty [Field], nullable - reflection field instance for the [thing]
+     * @param property [Field], nullable - reflection field instance for the [thing]
      * @return [Supplier]&lt;[T]&gt; supplier instance for retrieving values from the config scope provided.
      * @author fzzyhmstrs
      * @since 0.5.3
      */
-    fun supplierJava(scope: String, config: Config, thing: Any, property: Field?): Supplier<T>
+    @ApiStatus.Experimental
+    fun supplierJava(scope: String, args: List<String>, config: Config, thing: Any, property: Field?): Supplier<T>
 }
--- src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/loot/ConfigLootNumberProvider.kt
+++ src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/loot/ConfigLootNumberProvider.kt
@@ -1,22 +1,19 @@
 /*
-* Copyright (c) 2024 Fzzyhmstrs
-*
-* This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
-*
-* Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
-* You should have received a copy of the TDL-M with this software.
-* If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
-* */
+ * Copyright (c) 2024 Fzzyhmstrs
+ *
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+ *
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * You should have received a copy of the TDL-M with this software.
+ * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+ */
 
-package me.fzzyhmstrs.fzzy_config.loot
+package me.fzzyhmstrs.fzzy_config_test.loot
 
 import com.mojang.serialization.Codec
 import com.mojang.serialization.MapCodec
 import com.mojang.serialization.codecs.RecordCodecBuilder
-import me.fzzyhmstrs.fzzy_config.FC
-import me.fzzyhmstrs.fzzy_config.fcId
-import me.fzzyhmstrs.fzzy_config.impl.ConfigApiImpl
-import me.fzzyhmstrs.fzzy_config.result.ResultProviderSupplier
+import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import me.fzzyhmstrs.fzzy_config.validation.number.ValidatedNumber
 import net.minecraft.loot.context.LootContext
 import net.minecraft.loot.context.LootContextParameter
@@ -24,6 +21,7 @@ import net.minecraft.loot.provider.number.LootNumberProvider
 import net.minecraft.loot.provider.number.LootNumberProviderType
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
 import java.util.function.Supplier
 
 class ConfigLootNumberProvider(private val scope: String, private val scaling: Float): LootNumberProvider {
@@ -42,16 +40,16 @@ class ConfigLootNumberProvider(private val scope: String, private val scaling: F
 
     companion object {
 
-        private val resultProvider = ConfigApiImpl.createSimpleResultProvider(0f, ResultProviderSupplier { scope, _, thing, thingProp ->
+        private val resultProvider = ConfigApi.result().createSimpleResultProvider(0f) { scope, _, _, thing, thingProp ->
             when (thing) {
                 is ValidatedNumber<*> -> Supplier { (thing.get().toFloat()) }
-                is Number -> Supplier { ((thingProp.getter.call()as Number).toFloat()) }
+                is Number -> Supplier { ((thingProp.getter.call() as Number).toFloat()) }
                 else -> {
-                    FC.LOGGER.error("Error encountered while reading value for $scope. Value is not a number! Default value ${Supplier { 0f }} used.")
+                    println("Error encountered while reading value for $scope. Value is not a number! Default value ${Supplier { 0f }} used.")
                     Supplier { 0f }
                 }
             }
-        })
+        }
 
         private val CODEC: MapCodec<ConfigLootNumberProvider> = RecordCodecBuilder.mapCodec { instance: RecordCodecBuilder.Instance<ConfigLootNumberProvider> ->
             instance.group(
@@ -62,7 +60,7 @@ class ConfigLootNumberProvider(private val scope: String, private val scaling: F
 
         internal val TYPE = Registry.register(
             Registries.LOOT_NUMBER_PROVIDER_TYPE,
-            "config".fcId(),
+            Identifier.of("fzzy_config_test","config"),
             LootNumberProviderType(CODEC)
         )
 
