982138a0866fb8bc71991d52882db67c2e6971c9
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-10-12 17:47:16 -0400

    get result framework into an okie place for a beta launch, promote fc in total to release
    (cherry picked from commit 47b54097c4fa63b4ffe5c83736117293ad1648e8)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/networking/NetworkEventsClient.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/networking/NetworkEventsClient.kt
@@ -158,7 +158,8 @@ internal object NetworkEventsClient {
         FCC.withRestart { openRestartScreen ->
             if (openRestartScreen) {
                 ConfigApiImplClient.openRestartScreen()
-            }
+            } else
+                false
         }
     }
 
