e9a8ec0ad6377262b0f28a6fe63cf589168bdeaa
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-10-21 14:17:37 -0400

    (1.21) 0.5.5 reg manager

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/PortingUtils.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/PortingUtils.kt
@@ -35,7 +35,7 @@ object PortingUtils {
     }
 
     fun getDynamicManager(player: ServerPlayerEntity): RegistryWrapper.WrapperLookup {
-        return player.server.reloadableRegistries.createRegistryLookup().cast()
+        return player.server.reloadableRegistries.registryManager.cast()
     }
 
     fun <T> Registry<T>.optional(id: Identifier): Optional<T> {
