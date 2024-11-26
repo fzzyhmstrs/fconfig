6fbd88f693d50691ab64be8592e81417a3ee8330
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-11-26 09:18:18 -0500

    polish up registrar and impl a simple dev-only logger
    (cherry picked from commit 3c02061a0b75f807559322d8f834932d03c7e76a)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrySupplierImpl.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrySupplierImpl.kt
@@ -3,11 +3,16 @@ package me.fzzyhmstrs.fzzy_config.util.platform.impl
 import me.fzzyhmstrs.fzzy_config.util.platform.RegistrySupplier
 import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.entry.RegistryEntry
+import net.minecraft.util.Identifier
 
 class RegistrySupplierImpl<T>(private val entry: RegistryEntry.Reference<T>): RegistrySupplier<T> {
 
     override fun getKey(): RegistryKey<T> {
         return entry.registryKey()
+    }
+
+    override fun getId(): Identifier {
+        return entry.registryKey().value
     }
 
     override fun getEntry(): RegistryEntry<T> {
