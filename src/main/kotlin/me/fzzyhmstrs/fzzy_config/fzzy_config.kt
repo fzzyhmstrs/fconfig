33436046bd5ba3bf3bf7b6a984d729989e60d154
fzzyhmstrs <fzzyhmstrs@gmail.com>
2024-11-26 09:18:18 -0500

    polish up registrar and impl a simple dev-only logger
    (cherry picked from commit 3c02061a0b75f807559322d8f834932d03c7e76a)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/fzzy_config.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/fzzy_config.kt
@@ -10,10 +10,11 @@
 
 package me.fzzyhmstrs.fzzy_config
 
+import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import me.fzzyhmstrs.fzzy_config.networking.NetworkEvents
 import me.fzzyhmstrs.fzzy_config.networking.NetworkEventsClient
 import me.fzzyhmstrs.fzzy_config.networking.impl.NetworkApiImpl
-import me.fzzyhmstrs.fzzy_config.util.PlatformUtils
+import me.fzzyhmstrs.fzzy_config.util.platform.impl.PlatformUtils
 import net.minecraft.util.Identifier
 import net.neoforged.api.distmarker.Dist
 import net.neoforged.bus.api.IEventBus
@@ -69,6 +70,8 @@ class FzzyConfigNeoForge(bus: IEventBus, dist: Dist) {
 object FC {
     internal const val MOD_ID = "fzzy_config"
     internal val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
+    internal val DEVLOG: Logger = ConfigApi.platform().devLogger(MOD_ID)
+
 }
 
 /*@Mod(value = "fzzy_config")
--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrarImpl.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrarImpl.kt
@@ -4,11 +4,29 @@ import me.fzzyhmstrs.fzzy_config.nsId
 import me.fzzyhmstrs.fzzy_config.util.platform.Registrar
 import me.fzzyhmstrs.fzzy_config.util.platform.RegistrySupplier
 import net.minecraft.registry.Registry
+import net.minecraft.registry.tag.TagKey
+import net.minecraft.util.Identifier
 import java.util.function.Supplier
 
 class RegistrarImpl<T>(private val namespace: String, private val registry: Registry<T>): Registrar<T> {
 
+    override fun init() {
+        //fabric needs nothing
+    }
+
     override fun register(name: String, entrySupplier: Supplier<T>): RegistrySupplier<T> {
         return RegistrySupplierImpl(Registry.registerReference(registry, namespace.nsId(name), entrySupplier.get()))
+    }
+
+    override fun getRegistry(): Registry<T> {
+        return registry
+    }
+
+    override fun createTag(path: String): TagKey<T> {
+        return TagKey.of(registry.key, namespace.nsId(path))
+    }
+
+    override fun createTag(id: Identifier): TagKey<T> {
+        return TagKey.of(registry.key, id)
     }
 }
