95a161a9bc0aca3c5ddf85449f0471eceedd3b14
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2024-12-03 10:59:20 -0500

    Create ImmutableSuppliedPos.kt
    (cherry picked from commit 9a33ba5d29ab210f1bba959d962d8ac1477235d6)

---
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/pos/ImmutableSuppliedPos.kt
@@ -0,0 +1,35 @@
+/*
+* Copyright (c) 2024 Fzzyhmstrs
+*
+* This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
+*
+* Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+* You should have received a copy of the TDL-M with this software.
+* If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
+* */
+
+package me.fzzyhmstrs.fzzy_config.util.pos
+
+import java.util.function.Supplier
+
+/**
+ * A relative [Pos] wth a offset supplier. Offsets a parent Pos. This position is immutable in the direct sense. Mutation of this pos via [set], [inc], and [dec] does *not* affect position.
+ * @param parent Pos - the Pos this is relative to
+ * @param offset Supplier<Int> - the supplied offset compared to the parent
+ * @author fzzyhmstrs
+ * @since 0.5.9
+ */
+class ImmutableSuppliedPos(private val parent: Pos, private val offset: Supplier<Int>): Pos {
+    override fun get(): Int {
+        return parent.get() + offset.get()
+    }
+    override fun set(new: Int) {
+    }
+    override fun inc(amount: Int) {
+    }
+    override fun dec(amount: Int) {
+    }
+    override fun toString(): String {
+        return "[$parent + ${offset.get()}]"
+    }
+}
