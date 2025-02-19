dd1357fcaa0f3cad1859d0409e2c942ef5e57827
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-18 19:56:40 -0500

    0.6.5 get RegistrySupplier fully working as a RegEntry and fix up remote work
    (cherry picked from commit 665a78390af6294aaf281a165f3d89829fb6972d)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrySupplierImpl.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/impl/RegistrySupplierImpl.kt
@@ -5,11 +5,16 @@ import net.minecraft.registry.RegistryKey
 import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.util.Identifier
 import net.minecraftforge.registries.RegistryObject
+import java.util.*
 
 internal class RegistrySupplierImpl<T>(private val entry: RegistryObject<T>, private val key: RegistryKey<T>): RegistrySupplier<T> {
 
-    override fun getKey(): RegistryKey<T> {
+    override fun getRegistryKey(): RegistryKey<T> {
         return key
+    }
+
+    override fun getKey(): Optional<RegistryKey<T>> {
+        return Optional.of(key)
     }
 
     override fun getId(): Identifier {
