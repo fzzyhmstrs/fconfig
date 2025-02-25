a29b68bb193ccadef01064718fcef36eafd959a7
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-02-24 22:26:58 -0500

    0.6.5 remove unneeded multi manager, optimize imports and a bit of memory footprint
    (cherry picked from commit e3178c0f05cad12207b6168ee87bfdc838cd42f3)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/entry/EntryAnchor.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/entry/EntryAnchor.kt
@@ -10,7 +10,6 @@
 
 package me.fzzyhmstrs.fzzy_config.entry
 
-import me.fzzyhmstrs.fzzy_config.FC
 import me.fzzyhmstrs.fzzy_config.api.ConfigApi
 import me.fzzyhmstrs.fzzy_config.nullCast
 import me.fzzyhmstrs.fzzy_config.screen.decoration.Decorated
@@ -96,7 +95,6 @@ interface EntryAnchor {
         INLINE {
             override fun action(scope: String, anchorId: String): Runnable {
                 return Runnable {
-                    FC.DEVLOG.error(scope)
                     ConfigApi.openScreen(scope)
                     MinecraftClient.getInstance().currentScreen?.nullCast<ConfigScreen>()?.scrollToGroup(anchorId)
                 }
