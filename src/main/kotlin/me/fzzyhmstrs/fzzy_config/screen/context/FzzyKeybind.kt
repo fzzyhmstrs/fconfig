1f030851f0cc965fe37605f71a3ec6259128b003
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-03-01 09:26:50 -0500

    0.6.5 more work on keybinds
    (cherry picked from commit 261d99678a469371ab0582eb727eed52093b16c9)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/screen/context/FzzyKeybind.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/screen/context/FzzyKeybind.kt
@@ -18,6 +18,7 @@ import net.minecraft.text.Text
  * @author fzzyhmstrs
  * @since 0.6.5
  */
+@JvmDefaultWithoutCompatibility
 sealed interface FzzyKeybind: Relevant {
     /**
      * Override from [Relevant]. Determines if an input is relevant for the handler or not.
@@ -25,16 +26,46 @@ sealed interface FzzyKeybind: Relevant {
      * @since 0.6.5
      */
     override fun relevant(inputCode: Int, ctrl: Boolean, shift: Boolean, alt: Boolean): Boolean
+
     /**
      * Creates a [Text] representation of the current keybind
+     * @return Text representation of the keybind
      * @author fzzyhmstrs
      * @since 0.6.5
      */
     fun keybind(): Text
+
     /**
      * Copies the current keybind object where possible.
+     * @return [FzzyKeybind] deep copy of this keybind
      * @author fzzyhmstrs
      * @since 0.6.5
      */
     fun clone(): FzzyKeybind
+
+    /**
+     * Keybinds contained within this keybind.
+     * @return List&lt;[FzzyKeybind]&gt; the keybinds within this keybind. By default, simply this keybind
+     * @author fzzyhmstrs
+     * @since 0.6.5
+     */
+    fun containedKeybinds(): List<FzzyKeybind> {
+        return listOf(this)
+    }
+
+    /**
+     * Creates a compound keybind (multiple-choice) with [other]. If this compound is already compound, the new addition will be added as a further choice.
+     * @param other [FzzyKeybind] other keybind to compound with
+     * @return [FzzyKeybind] compounding this keybinds options with the other keybinds
+     * @author fzzyhmstrs
+     * @since 0.6.5
+     */
+    fun compoundWith(other: FzzyKeybind): FzzyKeybind {
+        val list = this.containedKeybinds() + other.containedKeybinds()
+        return when (list.size) {
+            0 -> FzzyKeybindUnbound
+            1 -> list[0]
+            else -> FzzyKeybindCompound(list)
+        }
+    }
 }
