979c67070dcc759a5f740a143beb6480f19c2172
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-03-13 22:11:30 -0400

    0.6.6 root config system hashed out. Fix disappearing scroll bar, add screen opening lock and new methods.
    (cherry picked from commit 82b5a33076ad80a469f2c35ab6954023241892ac)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/impl/ConfigSet.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/impl/ConfigSet.kt
@@ -12,4 +12,4 @@ package me.fzzyhmstrs.fzzy_config.impl
 
 import me.fzzyhmstrs.fzzy_config.config.Config
 
-internal data class ConfigSet(val active: Config, val base: Config, val clientOnly: Boolean)
+internal data class ConfigSet(val active: Config, val base: Config, val clientOnly: Boolean, val rootConfig: Boolean)
