2ffcc05fad576e8e858f50d1e6d53be96ecfe14d
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2025-03-19 14:02:04 -0400

    0.6.7 add JSON5 enum
    (cherry picked from commit f7630900e960174b07cdc191cc97f03d5d48609f)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/api/ConvertType.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/api/ConvertType.kt
@@ -16,15 +16,35 @@ import net.peanuuutz.tomlkt.TomlElement
 import java.util.function.Function
 
 /**
- * Defines the output file
+ * Defines the output file type used for the config. 
  * @author fzzyhmstrs
  * @since 0.6.7
  */
-enum class ConvertType(private val encoder: Function<TomlElement, ValidationResult<String>>, private val decoder: Function<String, ValidationResult<TomlElement>>) {
-    TOML(ConfigApiImpl::encodeToml, ConfigApiImpl::decodeToml),
-    JSON(ConfigApiImpl::encodeJson, ConfigApiImpl::decodeJson);
+enum class ConvertType(private val suffix: String, private val encoder: Function<TomlElement, ValidationResult<String>>, private val decoder: Function<String, ValidationResult<TomlElement>>) {
+    /**
+     * TOML output format. The standard output.
+     * @author fzzyhmstrs
+     * @since 0.6.7
+     */
+    TOML(".toml", ConfigApiImpl::encodeToml, ConfigApiImpl::decodeToml),
+    /**
+     * JSON output format. Uses GSON and codecs to parse the json to/from the TOML used internally.
+     * @author fzzyhmstrs
+     * @since 0.6.7
+     */
+    JSON(".json", ConfigApiImpl::encodeJson, ConfigApiImpl::decodeJson),
+    /**
+     * JSON5 output format. Uses Jankson and codecs to parse the json to/from the TOML used internally. Comments will be automatically carried over from any @Comment or @TomlComment annotations applied.
+     * @author fzzyhmstrs
+     * @since 0.6.7
+     */
+    JSON5(".json5", ConfigApiImpl::encodeJson5, ConfigApiImpl::decodeJson5);
     //JSON5(ConfigApiImpl::encodeJson5, ConfigApiImpl::decodeJson5);
 
+    fun suffix(): String {
+        return suffix
+    }
+    
     fun encode(input: TomlElement): ValidationResult<String>> {
         return encoder.apply(input)
     }
