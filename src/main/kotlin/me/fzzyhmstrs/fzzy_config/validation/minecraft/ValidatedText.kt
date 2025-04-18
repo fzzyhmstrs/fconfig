2e9f582f43f093c569a902eb62d5435a866514c4
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-04-18 13:24:59 -0400

    0.6.10 jot down some ideas and bits-n-bobs for ValidatedText

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/minecraft/ValidatedText.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/minecraft/ValidatedText.kt
@@ -10,8 +10,67 @@
 
 package me.fzzyhmstrs.fzzy_config.validation.minecraft
 
+import net.minecraft.text.OrderedText
+import net.minecraft.text.Style
+import net.minecraft.util.Formatting
+import java.util.function.UnaryOperator
+
 class ValidatedText {
 
-    class TextHolder
+    /*
+    * Text holder that stores a set of lines, each line holds 1 or more text objects, which store a single text representation instance
+    *
+    * Line isn't necessarily one "line" in the editor. More like a paragraph.
+    *
+    * Representations can be various kinds of text, Plain text, translations, keybinds, etc. Each has their own handler and is managed separately.
+    * - For example, translation, keybind, etc. would have some indicator that denotes it as a translation
+    * - They define how the cursor interacts with it, deletes it, selects it, etc.
+    * - Also defines what styling does to it.
+    * - Like translatable or keybind would apply style to the whole text fragment, while plain text you can select bits and style
+    * - which would break the fragment apart into individually styled fragments that would later be appended together when a Text object is constructed
+    *
+    * Interfacing the UI with the text holding will be a trick, since lines can be >1 line
+    * - Some sort of tracker class that knows positions of things in the widget. Like Array of Arrays of line# and X pos
+    * - sort of suffix array of positions
+    * - index -> LinePos
+    * - LinePos -> index
+    * - X -> LinePos
+    * LinePos a small util class
+    *
+    * Cursor = Fragment(index?) + LinePos
+    * */
+
+
+    class Holder {
+    }
+
+    class Paragraph {
+
+    }
+
+    interface Fragment {
+        fun delete(startPos: Int, endPos: Int): InteractResult
+        fun type(chr: Char, position: Int): InteractResult
+        fun format(formatting: Formatting, handler: InputHandler): InteractResult
+        fun styled(startPos: Int, endPos: Int, operator: UnaryOperator<Style>, handler: InputHandler): InteractResult
+        fun click(handler: InputHandler)
+        fun release(handler: InputHandler)
+
+        val content: OrderedText
+        val style: Style
+    }
+
+    enum class InteractResult(val changed: Boolean) {
+        DELETED(true),
+        CHANGED(true),
+        DEFAULT(false)
+    }
+
+    //the vertical line the item belongs to and the x position
+    data class LinePos(val line: Int, val pos: Int)
+
+    class InputHandler {
+
+    }
 
 }
