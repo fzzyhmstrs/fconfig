50e423c7f8bfb8fa187b6727dea51a09ca8991be
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-05-16 22:09:29 -0400

    error - some docs work and impl a translation builder for the Name/Desc/Prefix annotations
    (cherry picked from commit d3cc766172a2953ae67b2f9cd2f95f07523029e2)

--- src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImplAny.kt
+++ src/testmod/kotlin/me/fzzyhmstrs/fzzy_config_test/test/TestConfigImplAny.kt
@@ -13,9 +13,11 @@ package me.fzzyhmstrs.fzzy_config_test.test
 import me.fzzyhmstrs.fzzy_config.annotations.*
 import me.fzzyhmstrs.fzzy_config.api.SaveType
 import me.fzzyhmstrs.fzzy_config.config.Config
+import me.fzzyhmstrs.fzzy_config.util.Translatable
 import me.fzzyhmstrs.fzzy_config.util.TriState
 import me.fzzyhmstrs.fzzy_config.util.Walkable
 import net.minecraft.util.Identifier
+import net.peanuuutz.tomlkt.TomlComment
 
 class TestConfigImplAny: Config(Identifier("fzzy_config_test","test_config_any")) {
 
@@ -23,17 +25,29 @@ class TestConfigImplAny: Config(Identifier("fzzy_config_test","test_config_any")
         return SaveType.SEPARATE
     }
 
+    @Translatable.Name("My Test Object")
     @ClientModifiable
     var any1 = MyTestAny()
 
     //var bl1 = true
 
     class MyTestAny: Walkable {
+        @Translatable.Name("Test 1")
+        @Translatable.Desc("Test 1 Description; very cool and powerful")
         @RequiresAction(Action.RESTART)
         var test: Int = 5
+        @Translatable.Name("Test 2")
+        @Translatable.Name("Prueba Dos", "es_es")
         var test2: Double = 4.5
+        @Translatable.Name("Test 3")
+        @Comment("Do comments work?")
         var test3: String = "ggg"
+        @Translatable.Name("Test 4")
+        @TomlComment("Do TomlComments work?")
         var test4: Boolean = false
+        @Translatable.Name("Test 5")
+        @Translatable.Prefix("Test prefix that is very cool")
+        @Translatable.Prefix("Prefijo de prueba que es muy genial", "es_es")
         var test5: TriState = TriState.DEFAULT
     }
 }
