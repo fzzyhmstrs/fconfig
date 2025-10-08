9904f04c90d575abd601e5a476e689a78ec55678
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-10-07 23:35:30 -0400

    0.7.3 more integration of the new CustomWidget, including in CustomListWidget. Test!
    (cherry picked from commit 89b1f7d127a8ba11af7bab0e5cb74e77cf835c1c)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/screen/widget/custom/CustomWidget.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/screen/widget/custom/CustomWidget.kt
@@ -11,8 +11,11 @@
 package me.fzzyhmstrs.fzzy_config.screen.widget.custom
 
 import net.minecraft.client.gui.Click
+import net.minecraft.client.gui.Element
+import net.minecraft.client.gui.widget.ClickableWidget
 import net.minecraft.client.input.CharInput
 import net.minecraft.client.input.KeyInput
+import net.minecraft.client.input.MouseInput
 import net.minecraft.client.util.InputUtil
 import org.jetbrains.annotations.ApiStatus
 
@@ -97,6 +100,54 @@ interface CustomWidget {
         fun deltaY(): Double
         fun horizontalAmount(): Double
         fun verticalAmount(): Double
+
+        fun clickWidget(widget: Element): Boolean {
+            return clickWidgetOrNull(widget) ?: false
+        }
+
+        fun clickWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onMouse(this)
+            } else {
+                widget?.mouseClicked(Click(x(), y(), MouseInput(button(), modifiers())), double())
+            }
+        }
+
+        fun dragWidget(widget: Element): Boolean {
+            return dragWidgetOrNull(widget) ?: false
+        }
+
+        fun dragWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onMouseDrag(this)
+            } else {
+                widget?.mouseDragged(Click(x(), y(), MouseInput(button(), modifiers())), deltaX(), deltaY())
+            }
+        }
+
+        fun releaseWidget(widget: Element): Boolean {
+            return releaseWidgetOrNull(widget) ?: false
+        }
+
+        fun releaseWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onMouseRelease(this)
+            } else {
+                widget?.mouseReleased(Click(x(), y(), MouseInput(button(), modifiers())))
+            }
+        }
+
+        fun scrollWidget(widget: Element): Boolean {
+            return scrollWidgetOrNull(widget) ?: false
+        }
+
+        fun scrollWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onMouseScroll(this)
+            } else {
+                widget?.mouseScrolled(x(), y(), horizontalAmount(), verticalAmount())
+            }
+        }
     }
 
     class OnClick(private val click: Click, private val doubled: Boolean): MouseEvent {
@@ -243,6 +294,38 @@ interface CustomWidget {
         fun isEnterOrSpace(): Boolean {
             return this.key() == InputUtil.GLFW_KEY_ENTER || (this.key() == InputUtil.GLFW_KEY_SPACE) || (this.key() == InputUtil.GLFW_KEY_KP_ENTER)
         }
+
+        fun isLeft(): Boolean {
+            return key() == InputUtil.GLFW_KEY_LEFT
+        }
+
+        fun isRight(): Boolean {
+            return key() == InputUtil.GLFW_KEY_RIGHT
+        }
+
+        fun keyWidget(widget: Element): Boolean {
+            return keyWidgetOrNull(widget) ?: false
+        }
+
+        fun keyWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onKey(this)
+            } else {
+                widget?.keyPressed(KeyInput(key(), scancode(), modifiers()))
+            }
+        }
+
+        fun releaseWidget(widget: Element): Boolean {
+            return releaseWidgetOrNull(widget) ?: false
+        }
+
+        fun releaseWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onKeyRelease(this)
+            } else {
+                widget?.keyReleased(KeyInput(key(), scancode(), modifiers()))
+            }
+        }
     }
 
     class CharEvent(private val input: CharInput) {
@@ -252,6 +335,18 @@ interface CustomWidget {
         }
         fun modifiers(): Int {
             return input.modifiers
+        }
+
+        fun charWidget(widget: Element): Boolean {
+            return charWidgetOrNull(widget) ?: false
+        }
+
+        fun charWidgetOrNull(widget: Element?): Boolean? {
+            return if (widget is CustomWidget) {
+                widget.onChar(this)
+            } else {
+                widget?.charTyped(CharInput(codepoint(), modifiers()))
+            }
         }
     }
 }
