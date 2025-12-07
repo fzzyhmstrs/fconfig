79d099af43ce35fac87ec02db397368d9254f39c
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-12-06 22:12:42 -0500

    0.7.4 finish up basic RegistryBuilder implementation and docs
    (cherry picked from commit 1550d4f07e30b11660aa394f5fe1a91ac02aec7a)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/RegistryBuilder.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/platform/RegistryBuilder.kt
@@ -28,72 +28,134 @@ import net.minecraft.util.Identifier
 import net.minecraft.util.dynamic.Codecs
 import java.util.function.Function
 
-abstract class RegistryBuilder(private val namespace: String) {
+/**
+ * A utility for creating custom registries. Also includes some helper methods for interacting with registries
+ * @author fzzyhmstrs
+ * @since 0.7.4
+ */
+interface RegistryBuilder {
 
-    abstract fun <T: Any> build(key: RegistryKey<Registry<T>>): Registry<T>
+    /**
+     * Creates a Simple Registry.
+     *
+     * | Feature | Included |
+     * |---------|----------|
+     * | Default value | false |
+     * | Crashes if object not registered | false |
+     *
+     * @param T type of object to be stored in the registry
+     * @param key [RegistryKey] key for the registry being built
+     * @return built registry
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T: Any> build(key: RegistryKey<Registry<T>>): Registry<T>
 
-    abstract fun <T: Any> buildIntrusive(key: RegistryKey<Registry<T>>): Registry<T>
+    /**
+     * Creates an Intrusive Registry.
+     *
+     * | Feature | Included |
+     * |---------|----------|
+     * | Default value | false |
+     * | Crashes if object not registered | true |
+     *
+     * @param T type of object to be stored in the registry
+     * @param key [RegistryKey] key for the registry being built
+     * @return built registry
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T: Any> buildIntrusive(key: RegistryKey<Registry<T>>): Registry<T>
 
-    abstract fun <T: Any> buildDefaulted(key: RegistryKey<Registry<T>>, defaultId: Identifier): Registry<T>
+    /**
+     * Creates a Defaulted Registry.
+     *
+     * | Feature | Included |
+     * |---------|----------|
+     * | Default value | true |
+     * | Crashes if object not registered | false |
+     *
+     * @param T type of object to be stored in the registry
+     * @param key [RegistryKey] key for the registry being built
+     * @param defaultId [Identifier] id for the default value of the registry. This value needs to be registered separately.
+     * @return built registry
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T: Any> buildDefaulted(key: RegistryKey<Registry<T>>, defaultId: Identifier): Registry<T>
 
-    abstract fun <T: Any> buildDefaultedIntrusive(key: RegistryKey<Registry<T>>, defaultId: Identifier): Registry<T>
+    /**
+     * Creates a Defaulted Intrusive Registry.
+     *
+     * | Feature | Included |
+     * |---------|----------|
+     * | Default value | true |
+     * | Crashes if object not registered | true |
+     *
+     * @param T type of object to be stored in the registry
+     * @param key [RegistryKey] key for the registry being built
+     * @param defaultId [Identifier] id for the default value of the registry. This value needs to be registered separately.
+     * @return built registry
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T: Any> buildDefaultedIntrusive(key: RegistryKey<Registry<T>>, defaultId: Identifier): Registry<T>
 
-    abstract fun itemGroup(): ItemGroup.Builder
+    /**
+     * Makes a blank [ItemGroup.Builder] for creating an item group as needed.
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun itemGroup(): ItemGroup.Builder
 
-    abstract fun getTagName(tagKey: TagKey<*>): Text
+    /**
+     * Returns the translated name for a tag, where available.
+     * @return [Text] translated name for this tag. Falls back to `#namespace:path` if a translation isn't available
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun getTagName(tagKey: TagKey<*>): Text
 
-    fun <T> namespaceCodec(registry: Registry<T>): Codec<T> {
+    /**
+     * Creates a registry value codec that allows for identifier shortcutting based on the namespace used to build this builder. Vanilla will shorten ids for the `minecraft` namespace (`minecraft:thing` to `thing`). This does the same thing but for the namespace provided.
+     *
+     * This is useful for creating more concise data formats with many registered "keys" without having to resort to registering those keys to the MC namespace.
+     * @param T type of object stored in the registry
+     * @param registry [Registry] Registry to build the codec off of.
+     * @return [Codec] for values of [T] that will shorten output strings if the id namepace is the one you built this builder with
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T> namespaceCodec(registry: Registry<T>): Codec<T>
 
-        fun validateReference(entry: RegistryEntry<T>): DataResult<Reference<T>> {
-            val dataResult: DataResult<Reference<T>> = if (entry is Reference<T>) {
-                DataResult.success(entry)
-            } else {
-                DataResult.error { "Unregistered holder in " + registry.key.toString() + ": " + entry.toString() }
-            }
-            return dataResult
-        }
+    /**
+     * Creates a [RegistryEntry] codec that automatically handles [RegistrySupplier] properly
+     * @param T type of object stored in the registry
+     * @param registry [Registry] Registry to build the codec off of.
+     * @return [Codec] for [RegistryEntry] ot [T] that will properly parse [RegistrySupplier] without needing to unwrap it first. The decoded entry will *not* be a [RegistrySupplier] in that case. It will be a standard [RegistryEntry.Reference].
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T> regSupplierCodec(registry: Registry<T>): Codec<RegistryEntry<T>>
 
-        val idCodec: Codec<Identifier> = Codec.STRING.xmap(
-            { s -> if (!s.contains(':')) namespace.nsId(s) else s.simpleId() },
-            { i -> if(i.namespace == namespace) i.path else i.toString() }
-        )
+    /**
+     * Creates an entry codec for a registry that works with [RegistryEntry.Reference] directly, allowing you to work with the methods of that class directly.
+     * @param T type of object stored in the registry
+     * @param registry [Registry] Registry to build the codec off of.
+     * @return [Codec] for [RegistryEntry.Reference] ot [T]
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T> referenceEntryCodec(registry: Registry<T>): Codec<Reference<T>>
 
-        return idCodec.flatXmap(
-            {id -> registry.getEntry(id).map{ DataResult.success(it.value()) }.orElseGet{ DataResult.error<T> { "Unknown registry key in " + registry.key.toString() + ": " + id.toString() } }},
-            { value -> validateReference(registry.getEntry(value)).map { it.registryKey().value } }
-        )
-    }
-
-    companion object {
-
-        internal fun <T> Codec<RegistryEntry<T>>.regSupplierCodec(): Codec<RegistryEntry<T>> {
-            return this.xmap(Function.identity()) { re -> if (re is RegistrySupplier<T>) re.getEntry().cast() else re }
-        }
-
-        internal fun <T: Any> getReferenceEntryCodec(registry: Registry<T>): Codec<Reference<T>> {
-            val codec: Codec<Reference<T>> = Identifier.CODEC
-                .comapFlatMap(
-                    { id: Identifier ->
-                        registry.getEntry(id).map<DataResult<Reference<T>>> { result: Reference<T> ->
-                            DataResult.success(result)
-                        }.orElseGet { DataResult.error { "Unknown registry key in " + registry.key + ": " + id } }
-                    },
-                    { entry: Reference<T> -> entry.registryKey().value }
-                )
-            return Codecs.withLifecycle(codec) { entry: Reference<T> ->
-                registry.getEntryInfo(
-                    entry.registryKey()
-                ).map { obj: RegistryEntryInfo -> obj.lifecycle() }.orElse(Lifecycle.experimental()) as Lifecycle
-            }
-        }
-
-        internal fun <T> Registry<T>.getEntryId(entry: RegistryEntry<T>): Identifier? {
-            return if (entry is Reference) {
-                return entry.registryKey().value
-            } else {
-                this.getId(entry.value())
-            }
-        }
-    }
-
+    /**
+     * Helper method for optimally extracting ids out of a [RegistryEntry]
+     * @param [T] type of object stored in the registry
+     * @param registry [Registry] Registry to get the id out of.
+     * @param entry [RegistryEntry] the entry to get the id for.
+     * @return [Identifier], nullable. If the entry is a [RegistryEntry.Reference], it will directly return it's stored id, otherwise it will search the registry for it, possibly returning null if you supply a [RegistryEntry.Direct]
+     * @author fzzyhmstrs
+     * @since 0.7.4
+     */
+    fun <T> getEntryId(registry: Registry<T>, entry: RegistryEntry<T>): Identifier?
 }
