c5cf63a7ea22e1afb9d38569011245770aaccd2a
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-12-06 23:24:24 -0500

    0.7.4 add new popup types to ValidatedEnum, and now an Opener too
    (cherry picked from commit c0e3f0f5a77d686451142dfbe7f1ce1cff31d969)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedEnum.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedEnum.kt
@@ -11,14 +11,20 @@
 package me.fzzyhmstrs.fzzy_config.validation.misc
 
 import me.fzzyhmstrs.fzzy_config.entry.Entry
+import me.fzzyhmstrs.fzzy_config.entry.EntryOpener
 import me.fzzyhmstrs.fzzy_config.entry.EntryValidator
+import me.fzzyhmstrs.fzzy_config.screen.entry.WidgetEntry
+import me.fzzyhmstrs.fzzy_config.screen.widget.DynamicListWidget
+import me.fzzyhmstrs.fzzy_config.screen.widget.LayoutClickableWidget
 import me.fzzyhmstrs.fzzy_config.screen.widget.LayoutWidget
 import me.fzzyhmstrs.fzzy_config.screen.widget.PopupWidget
 import me.fzzyhmstrs.fzzy_config.screen.widget.custom.CustomPressableWidget
+import me.fzzyhmstrs.fzzy_config.screen.widget.internal.NavigableTextFieldWidget
 import me.fzzyhmstrs.fzzy_config.util.FcText
 import me.fzzyhmstrs.fzzy_config.util.FcText.descLit
 import me.fzzyhmstrs.fzzy_config.util.FcText.transLit
 import me.fzzyhmstrs.fzzy_config.util.FcText.translate
+import me.fzzyhmstrs.fzzy_config.util.Translatable
 import me.fzzyhmstrs.fzzy_config.util.ValidationResult
 import me.fzzyhmstrs.fzzy_config.validation.ValidatedField
 import me.fzzyhmstrs.fzzy_config.validation.misc.ValidatedEnum.WidgetType
@@ -46,9 +52,9 @@ import kotlin.math.max
  * @param defaultValue Enum Constant used as the default for this setting
  * @param widgetType [WidgetType] defines the GUI selection type. Defaults to POPUP
  * @author fzzyhmstrs
- * @since 0.2.0
+ * @since 0.2.0, implements EntryOpener as of 0.7.4
  */
-open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, private val widgetType: WidgetType = WidgetType.POPUP): ValidatedField<T>(defaultValue) {
+open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, private val widgetType: WidgetType = WidgetType.POPUP): ValidatedField<T>(defaultValue), EntryOpener {
 
     /**
      * A validated Enum Class, initialized with the enum class.
@@ -102,6 +108,23 @@ open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, 
             WidgetType.CYCLING -> {
                 CyclingOptionsWidget(choicePredicate, this)
             }
+            WidgetType.INLINE -> {
+                val layout = LayoutWidget.Builder().paddingBoth(0).spacingBoth(0).clampWidth(110).build()
+                val constants = this@ValidatedEnum.get().declaringJavaClass.enumConstants.mapNotNull {
+                    @Suppress("UNCHECKED_CAST")
+                    it as? T
+                }.filter { choicePredicate.validateEntry(it, EntryValidator.ValidationType.STRONG).isValid() }
+                for ((index, const) in constants.withIndex()) {
+                    val button = EnumOptionWidget(const, 110, {c -> (c as Enum<*>) != this@ValidatedEnum.get()}, { this@ValidatedEnum.accept(it); PopupWidget.pop() })
+                    layout.add("choice$index", button, LayoutWidget.Position.BELOW, LayoutWidget.Position.ALIGN_CENTER)
+                }
+                LayoutClickableWidget(0, 0, 110, 20 * constants.size, layout).withNarrationAppender { builder ->
+                    builder.put(NarrationPart.TITLE, "fc.validated_field.current".translate(this@ValidatedEnum.get().transLit(this@ValidatedEnum.get().name)).append(". "))
+                }
+            }
+            WidgetType.SCROLLABLE -> {
+                EnumScrollablePopupButtonWidget(this.translation(), choicePredicate)
+            }
         }
     }
 
@@ -131,6 +154,11 @@ open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, 
         return FcText.translatable(descriptionKey(), fallback ?: valuesMap.keys.toString())
     }
 
+    @Internal
+    override fun open(args: List<String>) {
+        openPopup(translation())
+    }
+
     /**
      * @suppress
      */
@@ -155,7 +183,19 @@ open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, 
          * @author fzzyhmstrs
          * @since 0.2.0
          */
-        CYCLING
+        CYCLING,
+        /**
+         * The enum constants are displayed inline in the setting screen, stacked on top of each other. This widget will take up more than one "normal" setting widget of height (unless you only have one constant, in which case... why?).
+         * @author fzzyhmstrs
+         * @since 0.7.4
+         */
+        INLINE,
+        /**
+         * The choices are displayed in a popup with a scrollable list widget displaying the choices. Up to 6 choices are displayed in the "window", with additional options available via scrolling.
+         * @author fzzyhmstrs
+         * @since 0.7.4
+         */
+        SCROLLABLE
     }
 
     private fun openEnumPopup(name: Text, choicePredicate: ChoiceValidator<T> = ChoiceValidator.any(), xPosition: BiFunction<Int, Int, Int> = PopupWidget.Builder.center(), yPosition: BiFunction<Int, Int, Int> = PopupWidget.Builder.center()) {
@@ -182,6 +222,59 @@ open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, 
         PopupWidget.push(builder.build())
     }
 
+    private fun openPopup(name: Text, choicePredicate: ChoiceValidator<T> = ChoiceValidator.any(), choiceOptionRunnable: Runnable = Runnable { }, xPosition: BiFunction<Int, Int, Int> = PopupWidget.Builder.center(), yPosition: BiFunction<Int, Int, Int> = PopupWidget.Builder.center()) {
+        val textRenderer = MinecraftClient.getInstance().textRenderer
+        var buttonWidth = 86
+        val constants = this@ValidatedEnum.get().declaringJavaClass.enumConstants.mapNotNull {
+            @Suppress("UNCHECKED_CAST")
+            it as? T
+        }.filter { choicePredicate.validateEntry(it, EntryValidator.ValidationType.STRONG).isValid() }
+        for (const in constants) {
+            buttonWidth = max(buttonWidth, textRenderer.getWidth(const.let { it.transLit(it.name) }))
+        }
+        buttonWidth = max(150, buttonWidth + 4)
+        val entries: MutableList<BiFunction<DynamicListWidget, Int, out DynamicListWidget.Entry>> = mutableListOf()
+        if (constants.size <= 6)
+            buttonWidth += 10
+        for ((index, const) in constants.withIndex()) {
+            val button = EnumOptionWidget(const, buttonWidth, {c -> (c as Enum<*>) != this@ValidatedEnum.get()}, { this@ValidatedEnum.accept(it); PopupWidget.pop() })
+            val n = const.transLit(const.name)
+            val desc = const.descLit("").takeIf { it.string != "" }
+            entries.add( BiFunction { list, _ ->
+                WidgetEntry(list, "enum$index", Translatable.createResult(n, desc), 20, button)
+            })
+        }
+        var listWidth = buttonWidth
+        val spec = if (entries.size > 6) {
+            listWidth += 10
+            DynamicListWidget.ListSpec(leftPadding = 0, rightPadding = 4, verticalPadding = 0, listNarrationKey = "fc.narrator.position.entry")
+        } else {
+            DynamicListWidget.ListSpec(leftPadding = 0, rightPadding = -6, verticalPadding = 0, listNarrationKey = "fc.narrator.position.entry")
+        }
+        val entryList = DynamicListWidget(MinecraftClient.getInstance(), entries, 0, 0, listWidth, 120, spec)
+
+        val builder = PopupWidget.Builder(name)
+        builder.add("enum_list", entryList, LayoutWidget.Position.BELOW, LayoutWidget.Position.ALIGN_LEFT)
+        if (entries.size > 6) {
+            val searchField = NavigableTextFieldWidget(MinecraftClient.getInstance().textRenderer, listWidth, 20, FcText.EMPTY)
+            searchField.setMaxLength(100)
+            searchField.text = ""
+            fun setColor(entries: Int) {
+                if(entries > 0)
+                    searchField.setEditableColor(-1)
+                else
+                    searchField.setEditableColor(-43691)
+            }
+            searchField.setChangedListener { s -> setColor(entryList.search(s)) }
+            searchField.setTooltip(Tooltip.of("fc.config.search.desc".translate()))
+            builder.add("choice_search", searchField, LayoutWidget.Position.BELOW, LayoutWidget.Position.ALIGN_JUSTIFY_WEAK)
+        }
+        builder.positionX(xPosition)
+        builder.positionY(yPosition)
+        builder.additionalNarration("fc.validated_field.current".translate(this@ValidatedEnum.get().transLit(this@ValidatedEnum.get().name)))
+        PopupWidget.push(builder.build())
+    }
+
     //client
     private inner class EnumPopupButtonWidget(private val name: Text, private val choicePredicate: ChoiceValidator<T>)
         : CustomPressableWidget(0, 0, 110, 20, FcText.EMPTY) {
@@ -196,6 +289,23 @@ open class ValidatedEnum<T: Enum<*>> @JvmOverloads constructor(defaultValue: T, 
 
         override fun onPress() {
             openEnumPopup(name, choicePredicate, PopupWidget.Builder.popupContext { w -> this.x + this.width/2 - w/2 }, PopupWidget.Builder.popupContext { this.y - 20 })
+        }
+    }
+
+    //client
+    private inner class EnumScrollablePopupButtonWidget(private val name: Text, private val choicePredicate: ChoiceValidator<T>)
+        : CustomPressableWidget(0, 0, 110, 20, FcText.EMPTY) {
+
+        override fun getMessage(): Text {
+            return this@ValidatedEnum.get().let { it.transLit(it.name) }
+        }
+
+        override fun appendClickableNarrations(builder: NarrationMessageBuilder?) {
+            builder?.put(NarrationPart.TITLE, this.getMessage())
+        }
+
+        override fun onPress() {
+            openPopup(name, choicePredicate, {  }, PopupWidget.Builder.popupContext { w -> this.x + this.width/2 - w/2 }, PopupWidget.Builder.popupContext { this.y - 20 })
         }
     }
 
