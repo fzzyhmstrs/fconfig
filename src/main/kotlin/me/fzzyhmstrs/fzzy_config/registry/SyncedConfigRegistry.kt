319a094aa309b49bcefff83df921062be51c4366
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2025-12-09 13:36:52 -0500

    0.7.4 Update SyncedConfigRegistry.kt
    (cherry picked from commit beb618cb9ce5af1e6d1264ffaba1b32d3f5ccee0)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/registry/SyncedConfigRegistry.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/registry/SyncedConfigRegistry.kt
@@ -59,7 +59,7 @@ internal object SyncedConfigRegistry {
         }
     }
 
-    internal fun syncedConfigs(): Map<String, SyncedConfigEntry> {
+    internal fun syncedConfigs(): Map<String, SyncedConfigEntry<*>> {
         return syncedConfigs
     }
 
@@ -298,7 +298,7 @@ internal object SyncedConfigRegistry {
     }
 
     @Synchronized
-    internal fun registerConfig(config: Config, registerType: RegisterType, configCreator: () -> Config) {
+    internal fun <T: Config> registerConfig(config: T, registerType: RegisterType, configCreator: () -> T) {
         val entry = SyncedConfigEntry(config, registerType == RegisterType.SERVER, configCreator)
         syncedConfigs[config.getId().toTranslationKey()] = entry
         ThreadUtils.register(entry)
@@ -307,7 +307,7 @@ internal object SyncedConfigRegistry {
 
     internal class QuarantinedUpdate(val playerUuid: UUID, val changeHistory: List<String>, val configId: String, val configString: String)
 
-    internal data class SyncedConfigEntry(override val config: Config, val server: Boolean, override val configCreator: () -> Config): ConfigEntry {
+    internal data class SyncedConfigEntry<T: Config>(override val config: T, val server: Boolean, override val configCreator: () -> T): ConfigEntry<T> {
 
         override val client: Boolean = false
 
