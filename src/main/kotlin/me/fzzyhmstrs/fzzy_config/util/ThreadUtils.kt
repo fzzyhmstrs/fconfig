0f65b09c75e596ba7192649433bbaa5b3a5af490
fzzyhmstrs <72876796+fzzyhmstrs@users.noreply.github.com>
2025-12-09 12:33:57 -0500

    Update ThreadUtils.kt
    (cherry picked from commit 6bf1f6b60692a35329df4b9a45e022688273b9a8)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/ThreadUtils.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/ThreadUtils.kt
@@ -41,7 +41,7 @@ internal object ThreadUtils {
 
     private val lock: ReentrantLock = ReentrantLock()
     private val watchService = FileSystems.getDefault().newWatchService()
-    private val configWatchers: HashMap<Path, ConfigEntry> = hashMapOf()
+    private val configWatchers: HashMap<Path, ConfigEntry<out Config>> = hashMapOf()
 
     /*
     * There will be a max of one actual config instance to care about.
@@ -69,7 +69,7 @@ internal object ThreadUtils {
     *
     * */
 
-    fun start(onUpdateEvent: (ConfigEntry, () -> ValidationResult<TomlTable>) -> Unit) {
+    fun start(onUpdateEvent: (ConfigEntry<out Config>, () -> ValidationResult<TomlTable>) -> Unit) {
         FILE_WATCHER.scheduleAtFixedRate( {
             var watchKey: WatchKey? = watchService.poll()
             while (watchKey != null) {
@@ -89,7 +89,7 @@ internal object ThreadUtils {
         FILE_WATCHER.shutdown()
     }
 
-    fun register(entry: ConfigEntry) {
+    fun register(entry: ConfigEntry<out Config>) {
         try {
             lock.lock()
             val file = entry.config.getDir()
