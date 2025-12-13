42827b8f5998c3e27d4ced9f4c99abe119ef64b1
fzzyhmstrs <fzzyhmstrs@gmail.com>
2025-12-13 17:25:16 -0500

    0.7.4 1.21.2 fix color clicking when mouse not over

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedColor.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/misc/ValidatedColor.kt
@@ -1043,6 +1043,7 @@ open class ValidatedColor: ValidatedField<ColorHolder>, EntryOpener {
         }
 
         override fun onMouse(event: CustomWidget.MouseEvent): Boolean {
+            if (!isMouseOver(event.x(), event.y())) return false
             mouseHasBeenClicked = true
             updateHL(event.x(), event.y())
             return true
