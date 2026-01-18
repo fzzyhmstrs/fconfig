58389aefea82df5a02cce873973e12a0a8c4c606
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-18 18:06:26 -0500

    0.7.5 added translation, description, and arbitrary providers. added ConfigDeprecated annotation
    (cherry picked from commit 3d7d133071ac7c33569c15763cd269c173c39df5)

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/collection/ValidatedChoiceList.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/validation/collection/ValidatedChoiceList.kt
@@ -45,7 +45,6 @@ import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.client.gui.widget.ClickableWidget
 import net.minecraft.text.MutableText
 import net.minecraft.text.Text
-import net.minecraft.util.math.ColorHelper
 import net.peanuuutz.tomlkt.TomlArrayBuilder
 import net.peanuuutz.tomlkt.TomlElement
 import net.peanuuutz.tomlkt.asTomlArray
@@ -79,12 +78,16 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
     defaultValues: List<T>,
     private val choices: List<T>,
     private val entryHandler: EntryHandler<T>,
-    private val translationProvider: BiFunction<T, String, MutableText> = BiFunction { t, _ -> t.transLit(t.toString()) },
-    private val descriptionProvider: BiFunction<T, String, Text> = BiFunction { t, _ -> t.descLit("") },
+    translationProvider: BiFunction<T, String, MutableText> = BiFunction { t, _ -> t.transLit(t.toString()) },
+    descriptionProvider: BiFunction<T, String, Text> = BiFunction { t, _ -> t.descLit("") },
     private val widgetType: WidgetType = WidgetType.POPUP)
     :
     ValidatedLazyField<List<T>>(defaultValues, { listOf() }), List<T>, EntryOpener
 {
+
+    private val entryTranslationProvider: BiFunction<T, String, MutableText> = translationProvider
+    private val entryDescriptionProvider: BiFunction<T, String, Text> = descriptionProvider
+
     init {
         if (!choices.containsAll(defaultValues))
             throw IllegalStateException("Default value list [$defaultValues] not within valid choice list [$choices]")
@@ -189,7 +192,7 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
      */
     override fun instanceEntry(): ValidatedChoiceList<T> {
         @Suppress("DEPRECATION")
-        return ValidatedChoiceList(copyStoredValue(), this.choices, this.entryHandler, this.translationProvider, this.descriptionProvider, this.widgetType)
+        return this.copyProvidersTo(ValidatedChoiceList(copyStoredValue(), this.choices, this.entryHandler, this.entryTranslationProvider, this.entryDescriptionProvider, this.widgetType))
     }
 
     @Internal
@@ -309,7 +312,7 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
         val textRenderer = MinecraftClient.getInstance().textRenderer
         var buttonWidth = textRenderer.getWidth(choiceListTitle)
         for (const in choices) {
-            buttonWidth = max(buttonWidth, textRenderer.getWidth(this.translationProvider.apply(const, this.translationKey())) + 8)
+            buttonWidth = max(buttonWidth, textRenderer.getWidth(this.entryTranslationProvider.apply(const, this.translationKey())) + 8)
         }
         builder.pushSpacing(UnaryOperator.identity()) { 0 }
         for ((index, const) in choices.withIndex()) {
@@ -340,7 +343,7 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
         var buttonWidth = textRenderer.getWidth(choiceListTitle)
         val entries: MutableList<BiFunction<DynamicListWidget, Int, out DynamicListWidget.Entry>> = mutableListOf()
         for (const in choices) {
-            buttonWidth = max(buttonWidth, textRenderer.getWidth(this.translationProvider.apply(const, this.translationKey())) + 8)
+            buttonWidth = max(buttonWidth, textRenderer.getWidth(this.entryTranslationProvider.apply(const, this.translationKey())) + 8)
         }
         if (choices.size <= 6)
             buttonWidth += 10
@@ -358,8 +361,8 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
                         }
                         this.accept(newList)
                     })
-                val name = this.translationProvider.apply(const, this.translationKey())
-                val desc = this.descriptionProvider.apply(const, this.descriptionKey()).takeIf { it.string != "" }
+                val name = this.entryTranslationProvider.apply(const, this.translationKey())
+                val desc = this.entryDescriptionProvider.apply(const, this.descriptionKey()).takeIf { it.string != "" }
                 WidgetEntry(list, "choice$index", Translatable.createResult(name, desc), 20, button)
             })
         }
@@ -449,10 +452,10 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
     // End List Interface //////////////////////////////
 
     //client
-    private class ChoiceWidget<T>(private val thisVal: T, width: Int, private val selectedPredicate: Predicate<T>, private val entry: ValidatedChoiceList<T>, private val valueApplier: Consumer<T>): CustomPressableWidget(0, 0, width, 20, entry.translationProvider.apply(thisVal, entry.translationKey())) {
+    private class ChoiceWidget<T>(private val thisVal: T, width: Int, private val selectedPredicate: Predicate<T>, private val entry: ValidatedChoiceList<T>, private val valueApplier: Consumer<T>): CustomPressableWidget(0, 0, width, 20, entry.entryTranslationProvider.apply(thisVal, entry.translationKey())) {
 
         init {
-            entry.descriptionProvider.apply(thisVal, entry.descriptionKey()).takeIf { it.string != "" }?.also { setTooltip(Tooltip.of(it)) }
+            entry.entryDescriptionProvider.apply(thisVal, entry.descriptionKey()).takeIf { it.string != "" }?.also { setTooltip(Tooltip.of(it)) }
         }
 
         override val textures: TextureProvider = TextureSet.Quad(tex, disabled, highlighted, "widget/button_disabled_highlighted".fcId())
@@ -477,7 +480,7 @@ open class ValidatedChoiceList<T> @JvmOverloads @Deprecated("Use toChoiceSet fro
         }
 
         override fun getMessage(): Text {
-            return entry.translationProvider.apply(thisVal, entry.translationKey())
+            return entry.entryTranslationProvider.apply(thisVal, entry.translationKey())
         }
 
         override fun getNarrationMessage(): MutableText {
