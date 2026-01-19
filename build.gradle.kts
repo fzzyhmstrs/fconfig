9b20020f832e4166de07c2105ab74573304d6929
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-01-19 11:06:37 -0500

    0.7.5 (forge) get coroutines applied manually

--- build.gradle.kts
+++ build.gradle.kts
@@ -86,7 +86,7 @@ dependencies {
     include("blue.endless:jankson:$janksonVersion")
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation(kotlin("reflect"))
 }
 
