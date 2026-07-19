016777740884bc59def8f97ba10a13d080af7d69
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-07-19 15:27:33 -0400

    0.7.7 tweak stringreader

--- src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/style/StringReader.kt
+++ src/main/kotlin/me/fzzyhmstrs/fzzy_config/util/style/StringReader.kt
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2025 Fzzyhmstrs
+ * Copyright (c) 2026 Fzzyhmstrs
  *
- * This file is part of Imbued Sorcery, a mod made for minecraft; as such it falls under the license of Imbued Sorcery.
+ * This file is part of Fzzy Config, a mod made for minecraft; as such it falls under the license of Fzzy Config.
  *
- * Imbued Sorcery is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
+ * Fzzy Config is free software provided under the terms of the Timefall Development License - Modified (TDL-M).
  * You should have received a copy of the TDL-M with this software.
  * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
  */
 
-package me.fzzyhmstrs.fzzy_config.util
+package me.fzzyhmstrs.fzzy_config.util.style
 
 import net.minecraft.util.StringIdentifiable
 import java.util.function.Predicate
