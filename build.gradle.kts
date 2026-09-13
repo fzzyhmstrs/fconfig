a1d688a75b0eca2ee2bbee400663e9a26b6e6518
fzzyhmstrs <fzzyhmstrs@gmail.com>
2026-09-13 10:05:01 -0400

    0.7.7 add in environments for CF

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,17 +8,9 @@
 * If you did not, see <https://github.com/fzzyhmstrs/Timefall-Development-Licence-Modified>.
 * */
 
-import com.matthewprenger.cursegradle.CurseArtifact
-import com.matthewprenger.cursegradle.CurseProject
-import com.matthewprenger.cursegradle.CurseRelation
-import com.matthewprenger.cursegradle.Options
 import net.fabricmc.loom.task.RemapJarTask
 import org.gradle.jvm.tasks.Jar
-import org.jetbrains.dokka.base.DokkaBase
-import org.jetbrains.dokka.base.DokkaBaseConfiguration
-import org.jetbrains.dokka.gradle.DokkaTask
-import org.jetbrains.dokka.versioning.VersioningConfiguration
-import org.jetbrains.dokka.versioning.VersioningPlugin
+import org.jetbrains.kotlin.cli.common.toBooleanLenient
 import java.net.URI
 
 plugins {
@@ -26,9 +18,8 @@ plugins {
     val kotlinVersion: String by System.getProperties()
     kotlin("jvm").version(kotlinVersion)
     kotlin("plugin.serialization") version "2.3.20"
-    id("com.modrinth.minotaur") version "2.+"
     id("org.jetbrains.dokka") version "2.1.0"
-    id("com.matthewprenger.cursegradle") version "1.4.0"
+    id("com.hypherionmc.modutils.modpublisher") version "2.2.3"
     id("org.moddedmc.wiki.toolkit") version "0.4.+"
     `maven-publish`
 }
@@ -181,8 +172,8 @@ tasks {
         targetCompatibility = javaVersion
         withSourcesJar()
     }
-    modrinth.get().group = "upload"
-    modrinthSyncBody.get().group = "upload"
+    //modrinth.get().group = "upload"
+    //modrinthSyncBody.get().group = "upload"
 }
 
 val testmodJar =  tasks.register("testmodJar", Jar::class) {
@@ -260,7 +251,7 @@ tasks.withType<DokkaTask>().configureEach {
 }
 
 
-if (System.getenv("MODRINTH_TOKEN") != null) {
+/*if (System.getenv("MODRINTH_TOKEN") != null) {
     modrinth {
         val releaseType: String by project
         val mcVersions: String by project
@@ -282,9 +273,50 @@ if (System.getenv("MODRINTH_TOKEN") != null) {
         }
         debugMode.set(uploadDebugMode.toBooleanStrictOrNull() ?: true)
     }
+}*/
+
+if (System.getenv("CURSEFORGE_TOKEN") != null || System.getenv("MODRINTH_TOKEN") != null) {
+    publisher {
+        val releaseType: String by project
+        val mcVersions: String by project
+        val uploadDebugMode: String by project
+        val loaderVersions: String by project
+
+        apiKeys {
+            curseforge(System.getenv("CURSEFORGE_TOKEN") ?: "")
+            modrinth(System.getenv("MODRINTH_TOKEN") ?: "")
+        }
+
+        debug.set(uploadDebugMode.toBooleanStrictOrNull() ?: true)
+        gameType.set("minecraft")
+        curseID.set("1005914")
+        modrinthID.set("fzzy-config")
+        versionType.set(releaseType)
+        changelog.set(log)
+        projectVersion.set("${project.version}")
+        displayName.set("${base.archivesName.get()}-${project.version}")
+        gameVersions.set(mcVersions.split(","))
+        loaders.set(loaderVersions.split(","))
+        curseEnvironment.set("both")
+
+        artifact.set(tasks.remapJar.get().archiveFile.get())
+
+        addAdditionalFile {
+            artifact(tasks.remapSourcesJar)
+            changelog("Source files for ${base.archivesName.get()}-${project.version}")
+            fileType("sources-jar")
+        }
+
+        modrinthDepends {
+            required("kotlin-for-forge")
+        }
+        curseDepends {
+            required("kotlin-for-forge")
+        }
+    }
 }
 
-if (System.getenv("CURSEFORGE_TOKEN") != null) {
+/*if (System.getenv("CURSEFORGE_TOKEN") != null) {
     curseforge {
         val releaseType: String by project
         val mcVersions: String by project
@@ -322,12 +354,13 @@ if (System.getenv("CURSEFORGE_TOKEN") != null) {
             debug = uploadDebugMode.toBooleanStrictOrNull() ?: true
         })
     }
-}
+}*/
 
 tasks.register("uploadAll") {
     group = "upload"
-    dependsOn(tasks.modrinth.get())
-    dependsOn(tasks.curseforge.get())
+    dependsOn(tasks.publishMod.get())
+    //dependsOn(tasks.modrinth.get())
+    //dependsOn(tasks.curseforge.get())
     dependsOn(tasks.publish.get())
 }
 
